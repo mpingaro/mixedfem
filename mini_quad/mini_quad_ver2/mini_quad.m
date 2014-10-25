194e20a0b650b9b4472152731a35beedc919b05f
mpingaro <marco.pingaro@gmail.com>
2014-10-25 12:00:56 +0200

    new numerical examples

---
+++ mini_quad/mini_quad_ver2/mini_quad.m
@@ -0,0 +1,45 @@
+
+%% --------------------------------------------------------------------- %%
+% ------------------------ MIXED ELEMENT MINI QUAD ---------------------- %
+% -----------------------   ( by Marco Pingaro )   ---------------------- %
+% Phd Student IUSS Pavia,                                                 %
+% mail  : marco.pingaro@iusspavia.it                                      %
+% ----------------------------------------------------------------------- %
+% The problem is : laplace(u) = f                                         %
+% Dirichlet condition is u_D = 0 on boundary domain                       %
+%                                                                         %
+% The Weak Formulation is:                                                %
+% a(epsilon(u),epsilon(v)) - b(p,div(u) ) = ( f,v )                       %
+%                          - b(q,div(p) ) = 0                             %
+% ----------------------------------------------------------------------- %
+
+%% INPUT DATI 
+clear all; close all; clc;
+length  = 5;                     % lunghezza trave
+heigth  = 1;                     % altezza trave
+young   = 50;                    % modulo di Young
+ndx     = 20;                    % numero suddivisioni in x
+ndy     =  6;                    % numero suddivisioni in y
+f(1,1) = 0;                      % load distribiuted direction x
+f(2,1) = -0.04;                  % load distribiuted direction y
+% ----------------------------------------------------------------------- %
+
+% Geometry
+[coordinates,element,bc1,bc2,bc3,bc4] = beam(length,heigth,ndx,ndy) ;
+nelem = size(element,1) ; 
+nnod  = size(coordinates,1) ;
+ngdld = 2*nnod+4*nelem ;
+ngdlp = nnod ;
+ngdlt = ngdld + ngdlp ;
+cf = young/1.5 ;
+% Assembly global system
+[K,load] = assembly(coordinates,element,cf,f) ; 
+
+% Dirichlet boundary conditions
+bc = [bc3] ;
+
+% Solve linear system
+[spost,pres] = solve(K,load,bc,ngdld,ngdlp) ;
+
+% Plot
+plotsol(coordinates,spost,pres,ndx,ndy,nnod) ;

cc173e7f7c6664531d6629eb172e4a6c5eb90c39
mpingaro <marco.pingaro@gmail.com>
2014-11-03 18:14:10 +0100

    folders new

---
+++ start_test/peers_quad_ver1/assembly.m
@@ -0,0 +1,39 @@
+% by Marco Pingaro
+
+function [K,F] = assembly(coor,el,mc,cf,f)
+
+nelem = size(el,1) ;
+nnod = size(coor,1) ;
+ngdls = max(max(mc)) ;
+
+%% Assembly the global matrix
+ngdlt = ngdls + 2*nelem + nnod ;
+K = sparse(ngdlt,ngdlt) ;
+A = sparse(ngdls,ngdls) ;
+B = sparse(ngdls,2*nelem) ;
+C = sparse(ngdls,nnod) ;
+F = sparse(ngdlt,1) ;
+for k = 1:nelem
+    %% Physic coordiantes points of the elements
+    p(1,[1 2]) = coor( el(k,1),[1 2]) ;
+    p(2,[1 2]) = coor( el(k,2),[1 2]) ;
+    p(3,[1 2]) = coor( el(k,3),[1 2]) ;
+    p(4,[1 2]) = coor( el(k,4),[1 2]) ;
+    s = mc(k,12) ;
+    % Compute the bilinear form a(sigma,tau), b(u, div(tau)) & the load (f,v) 
+    [AELEM,BELEM,CELEM,load] = stiffness_peers(p,f,s,cf) ;
+    for i = 1:11
+        for j = 1:11
+            A(mc(k,i),mc(k,j)) = A(mc(k,i),mc(k,j)) + AELEM(i,j) ;
+        end
+        for jj=1:4
+            C(mc(k,i),el(k,jj)) = C(mc(k,i),el(k,jj)) + CELEM(i,jj) ;
+        end
+        B(mc(k,i),2*(k-1)+[1, 2]) = BELEM(i,[1, 2]) ;
+    end
+    F(ngdls+2*(k-1)+[1, 2],1) = -load([1, 2],1) ;
+end
+% GLOBAL SYSTEM
+K = [A, B, C; B',sparse(2*nelem,2*nelem+nnod); C', sparse(nnod,2*nelem+nnod)] ;
+
+return

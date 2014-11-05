5f9a777b4be605067df07afdde344bb32a176e24
mpingaro <marco.pingaro@gmail.com>
2014-11-05 17:23:40 +0100

    fix an error in solve mini

--- mini_quad/mini_quad_ver1/solve.m
+++ mini_quad/mini_quad_ver1/solve.m
@@ -5,7 +5,8 @@ function [spost,pres] = solve(K,F,bc,nd,np)
 % ASSIGN BOUNDARY CONDITION (u = 0)
 for i = 1: size(bc,2)
     K(bc(1,i),:) = 0 ;
-    K(bc(1,i),bc(1,i)) = 1 ;
+    K(bc(1,i),bc(1,i)) = 1 ; 
+    F(bc(1,i),1) = 0 ;
 end
 
 % ASSIGN BOUNDARY CONDITION (p = t)
--- mini_quad/mini_quad_ver2/solve.m
+++ mini_quad/mini_quad_ver2/solve.m
@@ -6,6 +6,7 @@ function [spost,pres] = solve(K,F,bc,nd,np)
 for i = 1: size(bc,2)
     K(bc(1,i),:) = 0 ;
     K(bc(1,i),bc(1,i)) = 1 ;
+    F(bc(1,i),1) = 0 ;
 end
 
 % ASSIGN BOUNDARY CONDITION (p = t)
